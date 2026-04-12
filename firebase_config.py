a8a844afdd68d2ff5a4c3d3cb934b995378476fe
Lalasa <lalasa0613@gmail.com>
2026-04-12 14:32:15 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,7 +1,17 @@
+import os
+import json
 import firebase_admin
-from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("firebase-key.json")
+from firebase_admin import credentials, firestore
+from dotenv import load_dotenv
+
+load_dotenv()
+
+firebase_creds = os.getenv("FIREBASE_CREDENTIALS")
+
+cred_dict = json.loads(firebase_creds)
+cred = credentials.Certificate(cred_dict)
+
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
